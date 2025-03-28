33ebab8959f6a748ccf72c8ce9af48a82bc06db8
Dagim Demeke <dagim1077@gmail.com>
2025-03-27 19:58:32 -0500

    input adjusted

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ let discount = document.getElementById('discount');
 let submit = document.getElementById('submit');
 let discountPrice = document.getElementById('discountPrice');
 let saving = document.getElementById('saving')
-let x;
+let totalPrice;
+let discountAmount;
+
+    
+
 submit.onclick = function () {
+
     price = Number(price.value);
     discount = Number(discount.value);
+    
+    if (isNaN(price) || isNaN(discount) || price < 0 || discount < 0 || discount > 100) {
+        discountPrice.textContent = `Please enter a valid rate.`;
+        saving.textContent = ``;
+    }
+    else {
+        discountAmount = price * (discount/100);
+        totalPrice = price - discountAmount;
+        discountPrice.textContent = `Total price after discount is $${totalPrice}.`;
+        saving.textContent = `You saved $${discountAmount}.`;
+    }
 
-    discount = price * (discount/100);
-    x = price - discount;
-    discountPrice.textContent = `Total price after discount is $${x}.`;
-    saving.textContent = `You saved $${discount}.`
+    
 }
 
 
