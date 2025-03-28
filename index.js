568a4762a1dc33aafb0f644070d96c3be9afb4fe
Dagim Demeke <dagim1077@gmail.com>
2025-03-27 19:20:27 -0500

    Added saving section

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ let price = document.getElementById('price');
 let discount = document.getElementById('discount');
 let submit = document.getElementById('submit');
 let discountPrice = document.getElementById('discountPrice');
+let saving = document.getElementById('saving')
 let x;
 submit.onclick = function () {
     price = Number(price.value);
     discount = Number(discount.value);
 
-    x = price - price * (discount/100);
+    discount = price * (discount/100);
+    x = price - discount;
     discountPrice.textContent = `Total price after discount is $${x}.`;
+    saving.textContent = `You saved $${discount}.`
 }
 
 
-// When full price and discount rate is inserted, calculate the total price that will be paid. 
+ 
