5a0aa905138c67545e12fe709e3528d3b881f818
Dagim Demeke <dagim1077@gmail.com>
2025-03-28 11:21:02 -0500

    let price to let priceInput

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-let price = document.getElementById('price');
-let discount = document.getElementById('discount');
-let submit = document.getElementById('submit');
-let discountPrice = document.getElementById('discountPrice');
-let saving = document.getElementById('saving')
+let priceInput = document.getElementById('price');
+let discountInput = document.getElementById('discount');
+const submit = document.getElementById('submit');
+const discountPrice = document.getElementById('discountPrice');
+const saving = document.getElementById('saving')
 let totalPrice;
 let discountAmount;
 
-    
-
 submit.onclick = function () {
 
-    price = Number(price.value);
-    discount = Number(discount.value);
-    
+    price = Number(priceInput.value);
+    discount = Number(discountInput.value);
+
     if (isNaN(price) || isNaN(discount) || price < 0 || discount < 0 || discount > 100) {
-        discountPrice.textContent = `Please enter a valid rate.`;
+        discountPrice.textContent = `Entry is not valid.`;
         saving.textContent = ``;
     }
     else {
         discountAmount = price * (discount/100);
         totalPrice = price - discountAmount;
-        discountPrice.textContent = `Total price after discount is $${totalPrice}.`;
-        saving.textContent = `You saved $${discountAmount}.`;
+        discountPrice.textContent = `Total price after discount is $${totalPrice.toFixed(2)}`;
+        saving.textContent = `You saved $${discountAmount.toFixed(2)}`;
     }
 
-    
 }
+
 
 
  
